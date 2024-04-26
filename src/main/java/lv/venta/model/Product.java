2656bf2d7c81259868d6dc9dec0994c41e4ca836
Jēkabs Konošonoks <jacob.konosx@gmail.com>
2024-04-26 14:27:22 +0300

    _

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,11 @@
 package lv.venta.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,13 +18,26 @@ import lombok.ToString;
 //@NoArgsConstructor
 //@RequiredArgsConstructor
 @ToString
+@Table(name = "ProductTable")
+@Entity
 
 public class Product {
 	@Setter(value = AccessLevel.NONE)
+	@Column(name = "ID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	
+	@Column(name = "Title")
 	private String title;
+	
+	@Column(name = "Description")
 	private String description;
+	
+	@Column(name = "Price")
 	private float price;
+	
+	@Column(name = "Quantity")
 	private int quantity;
 
 	
